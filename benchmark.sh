79ceaa5058ba8109b312ba5873ed7ebae12884ef
Philipp Schirmer <philipp@schirmer-online.de>
2014-12-10 17:01:21 +0100

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -4,6 +4,6 @@ for f in *.sql
 do
     for i in `seq 1 10`;
     do  
-        echo -e "Duration: \e[33m\e[1m" $(~/Utils-Queries/get_times.sh $f) "ms \e[0m"
+        echo -e "Duration: \e[33m\e[1m" $(./get_times.sh $f) "ms \e[0m"
     done
 done
