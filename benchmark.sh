866b0552dcfadb5ce1003309fbecb2969ac59dac
Philipp Schirmer <philipp@schirmer-online.de>
2014-12-10 17:00:20 +0100

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-progress() {
-  pc=0;
-  while true
-    do
-      echo -n -e "[$pc sec]\033[0K\r"
-      sleep 1
-      ((pc++))
-    done
-}
-
-echo "Running Benchmark Queries... "
-{
-    for f in *.sql
-    do
-        echo -e "Running Query \e[1m$f\e[0m"
-        progress &
-        progPid=$!
+for f in *.sql
+do
+    for i in `seq 1 10`;
+    do  
         echo -e "Duration: \e[33m\e[1m" $(~/Utils-Queries/get_times.sh $f) "ms \e[0m"
-        echo "" ; kill -13 "$progPid";
     done
-}
+done
