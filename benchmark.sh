7853a3f75f084a362f377f497282ce7ffcdb5eab
Carl Ambroselli <git@carl-ambroselli.de>
2014-12-10 18:59:40 +0100

    Explanaition

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+
+progress() {
+  pc=0;
+  while true
+    do
+      echo -n -e "[$pc sec]\033[0K\r"
+      sleep 1
+      ((pc++))
+    done
+}
+
+clear;
+
+
 cd Benchmark-Queries
 for f in Level-*.sql
 do
+    echo "Running $f"
     for i in `seq 1 10`;
-    do  
+    do
+        progress &
+        progPid=$!
         echo -e "Duration: \e[33m\e[1m" $(../helpers/get_times.sh $f) "ms \e[0m"
+        kill -13 "$progPid";
     done
 done
