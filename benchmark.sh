f805960e45db42310aa180112e2d128aa2ccc8fb
Philipp Schirmer <philipp@schirmer-online.de>
2014-12-10 15:29:05 +0100

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+progress() {
+  pc=0;
+  while true
+    do
+      echo -n -e "[$pc sec]\033[0K\r"
+      sleep 1
+      ((pc++))
+    done
+}
+
 echo "Running Benchmark Queries... "
 {
-    cd Utils-Queries/
     for f in *.sql
     do
         echo -e "Running Query \e[1m$f\e[0m"
