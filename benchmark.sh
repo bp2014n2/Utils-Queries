408705ad3e6ee24e842a025030dc64766984e656
Carl Ambroselli <git@carl-ambroselli.de>
2014-12-15 12:05:38 +0100

    Added settings_changer

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOG_FILE=../logs/log.txt
+
 progress() {
   pc=0;
   while true
@@ -12,17 +14,17 @@ progress() {
 
 clear;
 
-
+echo "saving log to $LOG_FILE"
 cd Benchmark-Queries
 for f in Level-*.sql
 do
     sudo service postgresql restart;
-    echo "Running $f"
-    for i in `seq 1 10`;
+    echo "Running $f" | tee -a $LOG_FILE
+    for i in `seq 1 5`;
     do
         progress &
         progPid=$!
-        echo -e "Duration: \e[33m\e[1m" $(../helpers/get_times.sh $f) "ms \e[0m"
+        echo -e "Duration: \e[33m\e[1m" $(../helpers/get_times.sh $f) "ms \e[0m" | tee -a $LOG_FILE
         kill -13 "$progPid";
     done
 done
---
+++ helpers/settings_changer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+  then
+    echo -e "\e[31mUsage: $0 FIELD VALUE\e[0m\nexample: \e[32m$0 shared_buffers 4GB\e[0m"; exit;
+fi
+
+SETTINGS_FILE="/etc/postgresql/9.3/main/postgresql.conf"
+
+SET=$1
+VALUE=$2
+
+BACKUP="`grep "^$SET" $SETTINGS_FILE`"
+sudo sed -i -e "s/^$SET.*$/$SET = $VALUE/g" $SETTINGS_FILE
+
+../benchmark.sh
+
+sudo sed -i -e "s/^$SET.*$/$BACKUP/g" $SETTINGS_FILE
