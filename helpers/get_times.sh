12151cfee20bb27ee412b83177dec0ec4e8fe3d4
Carl Ambroselli <git@carl-ambroselli.de>
2014-12-16 11:41:09 +0100

    Get times returns now row_count

--- helpers/get_times.sh
+++ helpers/get_times.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-echo $(echo "`echo '\\\\timing \\\\\\\\ \\\\i' $1 | sudo -u postgres psql i2b2 2> /dev/null`" | grep 'Time: [0-9,.]* ms' | sed -e 's/Time: //g' | sed -e 's/ ms/ + /g' | tr -d '\n' | sed -e 's/+ $//g' | sed -e 's/,/\./g') | bc
+#echo $(
+result=$(echo "`echo '\\\\timing \\\\\\\\ \\\\i' $1 | sudo -u postgres psql i2b2 2> /dev/null`")
+echo -n "["
+echo "$result" | grep "         " | sed -e 's/ //g' | tr -d "\n";
+echo -n " rows] | "
+echo $(echo "$result" | grep 'Time: [0-9,.]* ms' | sed -e 's/Time: //g' | sed -e 's/ ms/ + /g' | tr -d '\n' | sed -e 's/+ $//g' | sed -e 's/,/\./g') | bc
